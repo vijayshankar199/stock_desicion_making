dc7aab194a350ba5400905d724df8a273c589c3e
vijaya sankar pitchuka <117157165+vijayshankar199@users.noreply.github.com>
2026-03-23 12:56:57 +0530

    Decrease database connect_timeout setting
    Reduced connect_timeout from 10 to 5 seconds.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,8 +93,7 @@ DATABASES = {
         'PORT': '3307',
         'CONN_MAX_AGE': 60,
         'OPTIONS': {
-            'connect_timeout': 10,
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'connect_timeout': 5,
         }
     }
 }
