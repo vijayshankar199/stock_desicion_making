36af37458bc2b147618c14567abe83c2386fe8fe
vijaya sankar pitchuka <117157165+vijayshankar199@users.noreply.github.com>
2026-03-23 12:14:12 +0530

    Add home view to serve a working response

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Working")
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('bot/',include("bot.urls")),
     path("stock/",include("Source.urls")),
     path("login/",include("login.urls")),
